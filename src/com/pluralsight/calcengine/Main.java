ea331d181cf70cd1f269358f2a969460f8c04812
roy-santos <roysantos112@gmail.com>
2019-11-30 18:44:00 -0800

    Use inheritance to abstract calculation details even further

--- src/com/pluralsight/calcengine/Main.java
+++ src/com/pluralsight/calcengine/Main.java
@@ -3,30 +3,62 @@ package com.pluralsight.calcengine;
 public class Main {
 
     public static void main(String[] args) {
-//        double[] leftVals = {100.0d, 25.0d, 225.0d, 11.0d};
-//        double[] rightVals = {50.0d, 92.0d, 17.0d, 3.0d};
-//        char[] opCodes = {'d', 'a', 's', 'm'};
-//        double[] results = new double[opCodes.length];
 
+        //creates array of MathEquations and sets the values of each element in the array
         MathEquation[] equations = new MathEquation[4];
-        equations[0] = create(100.0d, 50.0d, 'd');
-        equations[1] = create(25.0d, 92.0d, 'a');
-        equations[2] = create(225.0d, 17.0d, 's');
-        equations[3] = create(11.0d, 3.0d, 'm');
+        equations[0] = new MathEquation('d',100.0d, 50.0);
+        equations[1] = new MathEquation('a',25.0d, 92.0);
+        equations[2] = new MathEquation('s',225.0d, 17.0);
+        equations[3] = new MathEquation('m',11.0d, 3.0d);
 
         for(MathEquation equation : equations) {
             equation.execute();
             System.out.print("result = ");
             System.out.println(equation.getResult());
         }
-    }
 
-    public static MathEquation create(double leftVal, double rightVal, char opCode) {
-        MathEquation equation = new MathEquation();
-        equation.setLeftVal(leftVal);
-        equation.setRightVal(rightVal);
-        equation.setOpCode(opCode);
+        System.out.println();
+        System.out.println("Using Overloads");
+        System.out.println();
 
-        return equation;
+        double leftDouble = 9.0d;
+        double rightDouble = 4.0d;
+        int leftInt = 9;
+        int rightInt = 4;
+
+        MathEquation equationOverload = new MathEquation('d');
+
+        //uses overload execute() that accepts 2 double parameters)
+        equationOverload.execute(leftDouble, rightDouble);
+        System.out.print("result = ");
+        System.out.println(equationOverload.getResult());
+
+        //uses implicit type conversion to convert int to double
+        equationOverload.execute(leftInt, rightInt);
+        System.out.print("result = ");
+        System.out.println(equationOverload.getResult());
+
+        //system will choose method that accepts two doubles due to widening implicit conversion of 2nd parameter
+        equationOverload.execute((double) leftInt, rightInt);
+        System.out.print("result = ");
+        System.out.println(equationOverload.getResult());
+
+        //Using Inheritance for manageability and maintainability
+        System.out.println();
+        System.out.println("Using Inheritance");
+        System.out.println();
+
+        CalculateBase[] calculators= {
+                new Divider(100.0d, 50.d),
+                new Adder(25.0d, 92.0d),
+                new Subtracter(225.0d, 17.0d),
+                new Multiplier(11.0d, 3.0d)
+        };
+
+        for(CalculateBase calculator:calculators) {
+            calculator.calculate();
+            System.out.print("result = ");
+            System.out.println(calculator.getResult());
+        }
     }
 }
