a75f0832c59ad01b6fa2e7fed221947d992d7221
roy-santos <roysantos112@gmail.com>
2019-12-10 13:59:02 -0800

    Comment out unused classes and import statements. Add user input functionality via switch statement.

--- src/com/pluralsight/calcengine/CalculateHelper.java
+++ src/com/pluralsight/calcengine/CalculateHelper.java
@@ -1,108 +1,108 @@
-package com.pluralsight.calcengine;
-
-public class CalculateHelper {
-    private static final char ADD_SYMBOL = '+';
-    private static final char SUBTRACT_SYMBOL = '-';
-    private static final char MULTIPLY_SYMBOL = '*';
-    private static final char DIVIDE_SYMBOL = '/';
-
-    MathCommand command;
-    double leftValue;
-    double rightValue;
-    double result;
-
-    public void process(String statement) throws InvalidStatementException  {
-        //Breaks string down to individual parts
-        //example input: add 1.0 2.0
-        String[] parts = statement.split(" ");
-
-        //checks if input statement has 3 parts, otherwise throw exception
-        if(parts.length != 3)
-            throw new InvalidStatementException("Incorrect number of fields", statement);
-
-        String commandString = parts[0]; //add
-
-        //check if number format of input statement is correct, else throws exception
-        try {
-            leftValue = Double.parseDouble(parts[1]);
-            rightValue = Double.parseDouble(parts[2]);
-        } catch (NumberFormatException e) {
-            throw new InvalidStatementException("Non-numeric data", statement, e);
-        }
-
-        //converts commandString to corresponding enum type
-        setCommandFromString(commandString);
-
-        //check to see if command is still null, means error in command string
-        if(command == null) {
-            throw new InvalidStatementException("Invalid command", statement);
-        }
-
-        //calls correct derived CalculatorBase class based on enum type
-        CalculateBase calculator = null;
-        switch (command) {
-            case Add:
-                calculator = new Adder(leftValue, rightValue);
-                break;
-            case Subtract:
-                calculator = new Subtracter(leftValue, rightValue);
-                break;
-            case Multiply:
-                calculator = new Multiplier(leftValue, rightValue);
-                break;
-            case Divide:
-                calculator = new Divider(leftValue, rightValue);
-                break;
-        }
-
-        calculator.calculate();
-        result = calculator.getResult();
-    }
-
-    private void setCommandFromString(String commandString) {
-        //add -> MathCommand.Add
-
-        //converts enumeration to string value and compares it to the commandString
-        if(commandString.equalsIgnoreCase(MathCommand.Add.toString()))
-            command = MathCommand.Add;
-        else if(commandString.equalsIgnoreCase(MathCommand.Subtract.toString()))
-            command = MathCommand.Subtract;
-        else if(commandString.equalsIgnoreCase(MathCommand.Multiply.toString()))
-            command = MathCommand.Multiply;
-        else if(commandString.equalsIgnoreCase(MathCommand.Divide.toString()))
-            command = MathCommand.Divide;
-    }
-
-    //Override the object class' toString() method in order to print out the proper string when printing out a class instance.
-    @Override
-    public String toString() {
-        //1.0 + 2.0 = 3.0
-        char symbol = ' ';
-        switch (command) {
-            case Add:
-                symbol = ADD_SYMBOL;
-                break;
-            case Subtract:
-                symbol = SUBTRACT_SYMBOL;
-                break;
-            case Multiply:
-                symbol = MULTIPLY_SYMBOL;
-                break;
-            case Divide:
-                symbol = DIVIDE_SYMBOL;
-                break;
-        }
-
-        //Build output string
-        StringBuilder sb = new StringBuilder(20);
-        sb.append(leftValue);
-        sb.append(' ');
-        sb.append(symbol);
-        sb.append(' ');
-        sb.append(rightValue);
-        sb.append(" = ");
-        sb.append(result);
-
-        return sb.toString();
-    }
-}
+//package com.pluralsight.calcengine;
+//
+//public class CalculateHelper {
+//    private static final char ADD_SYMBOL = '+';
+//    private static final char SUBTRACT_SYMBOL = '-';
+//    private static final char MULTIPLY_SYMBOL = '*';
+//    private static final char DIVIDE_SYMBOL = '/';
+//
+//    MathCommand command;
+//    double leftValue;
+//    double rightValue;
+//    double result;
+//
+//    public void process(String statement) throws InvalidStatementException  {
+//        //Breaks string down to individual parts
+//        //example input: add 1.0 2.0
+//        String[] parts = statement.split(" ");
+//
+//        //checks if input statement has 3 parts, otherwise throw exception
+//        if(parts.length != 3)
+//            throw new InvalidStatementException("Incorrect number of fields", statement);
+//
+//        String commandString = parts[0]; //add
+//
+//        //check if number format of input statement is correct, else throws exception
+//        try {
+//            leftValue = Double.parseDouble(parts[1]);
+//            rightValue = Double.parseDouble(parts[2]);
+//        } catch (NumberFormatException e) {
+//            throw new InvalidStatementException("Non-numeric data", statement, e);
+//        }
+//
+//        //converts commandString to corresponding enum type
+//        setCommandFromString(commandString);
+//
+//        //check to see if command is still null, means error in command string
+//        if(command == null) {
+//            throw new InvalidStatementException("Invalid command", statement);
+//        }
+//
+//        //calls correct derived CalculatorBase class based on enum type
+//        CalculateBase calculator = null;
+//        switch (command) {
+//            case Add:
+//                calculator = new Adder(leftValue, rightValue);
+//                break;
+//            case Subtract:
+//                calculator = new Subtracter(leftValue, rightValue);
+//                break;
+//            case Multiply:
+//                calculator = new Multiplier(leftValue, rightValue);
+//                break;
+//            case Divide:
+//                calculator = new Divider(leftValue, rightValue);
+//                break;
+//        }
+//
+//        calculator.calculate();
+//        result = calculator.getResult();
+//    }
+//
+//    private void setCommandFromString(String commandString) {
+//        //add -> MathCommand.Add
+//
+//        //converts enumeration to string value and compares it to the commandString
+//        if(commandString.equalsIgnoreCase(MathCommand.Add.toString()))
+//            command = MathCommand.Add;
+//        else if(commandString.equalsIgnoreCase(MathCommand.Subtract.toString()))
+//            command = MathCommand.Subtract;
+//        else if(commandString.equalsIgnoreCase(MathCommand.Multiply.toString()))
+//            command = MathCommand.Multiply;
+//        else if(commandString.equalsIgnoreCase(MathCommand.Divide.toString()))
+//            command = MathCommand.Divide;
+//    }
+//
+//    //Override the object class' toString() method in order to print out the proper string when printing out a class instance.
+//    @Override
+//    public String toString() {
+//        //1.0 + 2.0 = 3.0
+//        char symbol = ' ';
+//        switch (command) {
+//            case Add:
+//                symbol = ADD_SYMBOL;
+//                break;
+//            case Subtract:
+//                symbol = SUBTRACT_SYMBOL;
+//                break;
+//            case Multiply:
+//                symbol = MULTIPLY_SYMBOL;
+//                break;
+//            case Divide:
+//                symbol = DIVIDE_SYMBOL;
+//                break;
+//        }
+//
+//        //Build output string
+//        StringBuilder sb = new StringBuilder(20);
+//        sb.append(leftValue);
+//        sb.append(' ');
+//        sb.append(symbol);
+//        sb.append(' ');
+//        sb.append(rightValue);
+//        sb.append(" = ");
+//        sb.append(result);
+//
+//        return sb.toString();
+//    }
+//}
--- src/com/pluralsight/myapp/Main.java
+++ src/com/pluralsight/myapp/Main.java
@@ -1,24 +1,26 @@
 package com.pluralsight.myapp;
 
 import com.pluralsight.calcengine.Adder;
-import com.pluralsight.calcengine.CalculateBase;
-import com.pluralsight.calcengine.CalculateHelper;
+// import com.pluralsight.calcengine.CalculateBase;
+// import com.pluralsight.calcengine.CalculateHelper;
 import com.pluralsight.calcengine.Divider;
 import com.pluralsight.calcengine.DynamicHelper;
-import com.pluralsight.calcengine.InvalidStatementException;
-import com.pluralsight.calcengine.MathEquation;
+// import com.pluralsight.calcengine.InvalidStatementException;
+// import com.pluralsight.calcengine.MathEquation;
 import com.pluralsight.calcengine.MathProcessing;
 import com.pluralsight.calcengine.Multiplier;
 import com.pluralsight.calcengine.PowerOf;
 import com.pluralsight.calcengine.Subtracter;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        //useMathEquation();
-        //useCalculatorBase();
-        //useCalculateHelper();
+ //     useMathEquation();
+ //     useCalculatorBase();
+ //     useCalculateHelper();
 
         String[] statements = {
                 "divide 100.0 50.0",    // 100.0 / 50.0 = 2.0
@@ -37,93 +39,134 @@ public class Main {
                 new Subtracter()
         });
 
+        // Create new scanner
+        Scanner scanner = new Scanner(System.in);
+
+        // Prompt user for input
+        System.out.print("Enter first number: ");
+        double leftVal = scanner.nextDouble();
+
+        System.out.print("Enter second number: ");
+        double rightVal = scanner.nextDouble();
+
+        System.out.print("Enter an operator (+, -, *, /, ^): ");
+        char operator = scanner.next().charAt(0);
+
+        switch(operator)
+        {
+            case '+':
+                System.out.println(helper.process("add " + leftVal + " " + rightVal));
+                break;
+
+            case '-':
+                System.out.println(helper.process("subtract " + leftVal + " " + rightVal));
+                break;
+
+            case '*':
+                System.out.println(helper.process("multiply " + leftVal + " " + rightVal));
+                break;
+
+            case '/':
+                System.out.println(helper.process("divide " + leftVal + " " + rightVal));
+                break;
+
+            case '^':
+                System.out.println(helper.process("power " + leftVal + " " + rightVal));
+                break;
+
+            default:
+                System.out.printf("You have entered wrong operator");
+                return;
+        }
+
+        // For processing the array of string statements
         for(String statement: statements) {
             String output = helper.process(statement);
             System.out.println(output);
         }
     }
 
-    static void useCalculateHelper() {
-        String[] statements = {
-                "add 1.0",               // Error: incorrect number of values
-                "add xx 25.0",           // Error: non-numeric data
-                "addX 0.0 0.0" ,         // Error: invalid command
-                "divide 100.0 50.0",    // 100.0 / 50.0 = 2.0
-                "add 25.0 92.0",        // 25.0 + 92.0 = 117.0
-                "subtract 225.0 17.0",  // 225.0 - 17.0 = 108.0
-                "multiply 11.0 3.0"    // 11.0 * 3.0 = 33.0
-        };
-
-        CalculateHelper helper = new CalculateHelper();
-        for (String statement : statements) {
-            try {
-                helper.process(statement);
-                System.out.println(helper);
-            } catch(InvalidStatementException e) {
-                System.out.println(e.getMessage());
-                if(e.getCause() != null)
-                    System.out.println("  Original exception: " + e.getCause().getMessage());
-            }
-        }
-    }
-
-    //creates array of MathEquations and sets the values of each element in the array
-    static void useMathEquation() {
-        MathEquation[] equations = new MathEquation[4];
-        equations[0] = new MathEquation('d', 100.0d, 50.0);
-        equations[1] = new MathEquation('a', 25.0d, 92.0);
-        equations[2] = new MathEquation('s', 225.0d, 17.0);
-        equations[3] = new MathEquation('m', 11.0d, 3.0d);
-
-        for (MathEquation equation : equations) {
-            equation.execute();
-            System.out.print("result = ");
-            System.out.println(equation.getResult());
-        }
-
-        System.out.println();
-        System.out.println("Using Overloads");
-        System.out.println();
-
-        double leftDouble = 9.0d;
-        double rightDouble = 4.0d;
-        int leftInt = 9;
-        int rightInt = 4;
-
-        MathEquation equationOverload = new MathEquation('d');
-
-        //uses overload execute() that accepts 2 double parameters)
-        equationOverload.execute(leftDouble, rightDouble);
-        System.out.print("result = ");
-        System.out.println(equationOverload.getResult());
-
-        //uses implicit type conversion to convert int to double
-        equationOverload.execute(leftInt, rightInt);
-        System.out.print("result = ");
-        System.out.println(equationOverload.getResult());
-
-        //system will choose method that accepts two doubles due to widening implicit conversion of 2nd parameter
-        equationOverload.execute((double) leftInt, rightInt);
-        System.out.print("result = ");
-        System.out.println(equationOverload.getResult());
-
-        //Using Inheritance for manageability and maintainability
-        System.out.println();
-        System.out.println("Using Inheritance");
-        System.out.println();
-    }
-
-    static void useCalculatorBase() {
-        CalculateBase[] calculators = {
-                new Divider(100.0d, 50.d),
-                new Adder(25.0d, 92.0d),
-                new Subtracter(225.0d, 17.0d),
-                new Multiplier(11.0d, 3.0d)
-        };
-        for(CalculateBase calculator:calculators) {
-            calculator.calculate();
-            System.out.print("result = ");
-            System.out.println(calculator.getResult());
-        }
-    }
+//    static void useCalculateHelper() {
+//        String[] statements = {
+//                "add 1.0",               // Error: incorrect number of values
+//                "add xx 25.0",           // Error: non-numeric data
+//                "addX 0.0 0.0" ,         // Error: invalid command
+//                "divide 100.0 50.0",    // 100.0 / 50.0 = 2.0
+//                "add 25.0 92.0",        // 25.0 + 92.0 = 117.0
+//                "subtract 225.0 17.0",  // 225.0 - 17.0 = 108.0
+//                "multiply 11.0 3.0"    // 11.0 * 3.0 = 33.0
+//        };
+//
+//        CalculateHelper helper = new CalculateHelper();
+//        for (String statement : statements) {
+//            try {
+//                helper.process(statement);
+//                System.out.println(helper);
+//            } catch(InvalidStatementException e) {
+//                System.out.println(e.getMessage());
+//                if(e.getCause() != null)
+//                    System.out.println("  Original exception: " + e.getCause().getMessage());
+//            }
+//        }
+//    }
+//
+//    //creates array of MathEquations and sets the values of each element in the array
+//    static void useMathEquation() {
+//        MathEquation[] equations = new MathEquation[4];
+//        equations[0] = new MathEquation('d', 100.0d, 50.0);
+//        equations[1] = new MathEquation('a', 25.0d, 92.0);
+//        equations[2] = new MathEquation('s', 225.0d, 17.0);
+//        equations[3] = new MathEquation('m', 11.0d, 3.0d);
+//
+//        for (MathEquation equation : equations) {
+//            equation.execute();
+//            System.out.print("result = ");
+//            System.out.println(equation.getResult());
+//        }
+//
+//        System.out.println();
+//        System.out.println("Using Overloads");
+//        System.out.println();
+//
+//        double leftDouble = 9.0d;
+//        double rightDouble = 4.0d;
+//        int leftInt = 9;
+//        int rightInt = 4;
+//
+//        MathEquation equationOverload = new MathEquation('d');
+//
+//        //uses overload execute() that accepts 2 double parameters)
+//        equationOverload.execute(leftDouble, rightDouble);
+//        System.out.print("result = ");
+//        System.out.println(equationOverload.getResult());
+//
+//        //uses implicit type conversion to convert int to double
+//        equationOverload.execute(leftInt, rightInt);
+//        System.out.print("result = ");
+//        System.out.println(equationOverload.getResult());
+//
+//        //system will choose method that accepts two doubles due to widening implicit conversion of 2nd parameter
+//        equationOverload.execute((double) leftInt, rightInt);
+//        System.out.print("result = ");
+//        System.out.println(equationOverload.getResult());
+//
+//        //Using Inheritance for manageability and maintainability
+//        System.out.println();
+//        System.out.println("Using Inheritance");
+//        System.out.println();
+//    }
+//
+//    static void useCalculatorBase() {
+//        CalculateBase[] calculators = {
+//                new Divider(100.0d, 50.d),
+//                new Adder(25.0d, 92.0d),
+//                new Subtracter(225.0d, 17.0d),
+//                new Multiplier(11.0d, 3.0d)
+//        };
+//        for(CalculateBase calculator:calculators) {
+//            calculator.calculate();
+//            System.out.print("result = ");
+//            System.out.println(calculator.getResult());
+//        }
+//    }
 }
